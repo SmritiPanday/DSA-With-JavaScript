6932b67e5a0df256f6ea2a1700db777c52e1ed54
smrti <rajsimmi369@gmail.com>
2024-06-29 20:42:28 +0530

    Big O of Objects example

---
+++ bigOof objects/index.js
@@ -0,0 +1,18 @@
+let myObj ={
+    name: 'smriti',
+    age:21,
+    city:'Patna'
+}
+
+
+// Big O of Objects
+// bject.keys( myObj)
+// Array(3) [ "name", "age", "city" ]
+
+// Object.values( myObj)
+// Array(3) [ "smriti", 21, "Patna" ]
+
+// Object.entries( myObj)
+// Array(3) [ (2) […], (2) […], (2) […] ]
+// Object.hasOwnProperty('name')
+// true 
