fbd9a925ca8af7f20b9724400e73c75496d0ca05
smrti <rajsimmi369@gmail.com>
2024-07-01 06:32:00 +0530

    Travering of an array

---
+++ Array/index.js
@@ -0,0 +1,10 @@
+//travering
+//1,2,3,4,5
+let arr=[1,2,3,4,5]
+function myArray(arr){
+    for(let i=0;i<arr.length;i++){
+      console.log(arr[i])
+    }
+}
+myArray(arr)
+//this is traversing of whole elements in an arrays 
